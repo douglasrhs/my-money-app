47b8d8f3f776f6d250faae56f0f4dfb4ae9b394f
Douglas Santos <douglas-rhs@hotmail.com>
2017-08-09 12:01:08 -0300

    New functions for include, removing Credts and Debts

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -11,8 +11,7 @@ import TabHeader from '../common/tab/tabHeader'
 import TabsHeader from '../common/tab/tabsHeader'
 import TabContent from '../common/tab/tabContent'
 import TabsContent from '../common/tab/tabsContent'
-import { selectTab, showTabs } from '../common/tab/tabActions'
-import { create, update, remove } from './billingCycleActions'
+import { init, create, update, remove } from './billingCycleActions'
 
 import List from './billingCycleList'
 import Form from './billingCycleForm'
@@ -20,8 +19,7 @@ import Form from './billingCycleForm'
 class BillingCycle extends Component {
 
     componentWillMount(){
-        this.props.selectTab('tabList')
-        this.props.showTabs('tabList','tabCreate')
+        this.props.init()
     }
 
     render() {
@@ -42,13 +40,16 @@ class BillingCycle extends Component {
                                 <List/>
                             </TabContent>
                             <TabContent id='tabCreate'>
-                                <Form onSubmit={this.props.create}/>
+                                <Form onSubmit={this.props.create}
+                                submitLabel='Incluir' submitClass='primary'/>
                             </TabContent>
                             <TabContent id='tabUpdate'>
-                                <Form onSubmit={this.props.update} />
+                                <Form onSubmit={this.props.update} 
+                                submitLabel='Alterar' submitClass='info'/>
                             </TabContent>
                             <TabContent id='tabDelete'>
-                                <Form onSubmit={this.props.remove} readOnly={true}/>
+                                <Form onSubmit={this.props.remove} readOnly={true}
+                                submitLabel='Excluir' submitClass='danger'/>
                             </TabContent>
                         </TabsContent>
                     </Tabs>
@@ -58,5 +59,5 @@ class BillingCycle extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({selectTab, showTabs, create, update, remove}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({init, create, update, remove}, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
